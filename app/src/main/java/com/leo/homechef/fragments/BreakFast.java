710159c1b627aee2136663a85e66eb00568bcaa5
nikhilepparla <e.nikhil1999@gmail.com>
2023-07-02 13:54:08 +0530

    Home ingredients initial idea

---
+++ app/src/main/java/com/leo/homechef/fragments/BreakFast.java
@@ -0,0 +1,22 @@
+package com.leo.homechef.fragments;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A fragment representing a list of Items.
+ */
+public class BreakFast extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
